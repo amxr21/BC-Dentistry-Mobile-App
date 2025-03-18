d54c4abd6f4068a26c5539db692a18f298fb51cd
Ammar Obad Al Wasim <ammar211080@gmail.com>
2025-03-18 15:09:44 +0400

    .

--- components/StatusUpdateLoading.jsx
+++ components/StatusUpdateLoading.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const StatusUpdateLoading = ({reff, status}) => {
   return (
     <View ref={reff} className={`bg-white ${status} ${status != "min-h-0 h-0" ? 'p-8' : "p-0"} flex items-center justify-center gap-4 w-full absolute left-0 top-0 z-50 overflow-hidden`}>
-        <View className={`bg-red-500 rounded-full ${status != "min-h-0 h-0" ? 'w-16 h-16' : "h-0 w-0"}`}>a</View>
+        <View className={`bg-red-500 rounded-full ${status != "min-h-0 h-0" ? 'w-16 h-16' : "h-0 w-0"}`}></View>
         <Text>Updating your request Status...</Text>
     </View>
   )
