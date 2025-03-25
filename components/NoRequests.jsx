7e996c7a001e06562e3b74dd0175202a7f6f4fbe
amxr21 <ammar211080@gmail.com>
2025-03-26 00:25:11 +0400

    update no requests component #2

--- components/NoRequests.jsx
+++ components/NoRequests.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const NoRequests = () => {
   return (
-    <View className='p-10 flex flex-col items-center rounded-2xl border-[0.5px] border-gray-300'>
-      <Text className='text-8xl font-black text-gray-400   leading-[3rem]'>...</Text>
+    <View className='p-8 flex flex-col items-center rounded-2xl border-[0.5px] bg-gray-50 border-gray-300'>
+      <Text className='text-8xl font-black text-gray-400 leading-[3rem]'>...</Text>
       <Text className=' text-gray-400'>All done, you don't have any pending requests!</Text>
     </View>
   )
